059f57a987e07d9bcbb509b83d002b9fc1b3fb28
MUZI0960 <giy09060@gmail.com>
2025-04-17 21:21:56 +0900

    인프런 강의 백엔드 4/17

--- src/main/java/org/muzi/apiserver/config/CustomServletConfig.java
+++ src/main/java/org/muzi/apiserver/config/CustomServletConfig.java
@@ -4,6 +4,7 @@ import lombok.extern.log4j.Log4j2;
 import org.muzi.apiserver.controller.formatter.LocalDateFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -13,11 +14,14 @@ public class CustomServletConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
 
-        log.info("-------------------------------------");
-        log.info("addFormatters");
-
         registry.addFormatter(new LocalDateFormatter());
     }
 
-
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")      // 어떤 경로에 설정할겁니까?
+                .maxAge(500) // 연결 최대 허용 시간
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedOrigins("*") ;  // 어디에서 들어오는 것을 허락?
+    }
 }
--- src/main/java/org/muzi/apiserver/controller/TodoController.java
+++ src/main/java/org/muzi/apiserver/controller/TodoController.java
@@ -33,13 +33,30 @@ public class TodoController {
     }
 
     @PostMapping("/")
-    public Map<String, Long> register(@RequestBody TodoDTO dto){
-        log.info("todoDTO :  " + dto);
+    public Map<String, Long> register(@RequestBody TodoDTO todoDTO){
+        log.info("todoDTO :  " + todoDTO);
 
-        Long tno = todoService.register(dto);
+        Long tno = todoService.register(todoDTO);
 
         return Map.of("TNO", tno);
 
+    }
+
+    @PutMapping("/{tno}")
+    public Map<String, String> modify(@PathVariable Long tno, @RequestBody TodoDTO todoDTO){
+        todoDTO.setTno(tno);
+
+        todoService.modify(todoDTO);
+
+        return Map.of("RESULT", "SUCCESS");
+    }
+
+    @DeleteMapping("/{tno}")
+    public Map<String, String> remove(@PathVariable Long tno){
+
+        todoService.remove(tno);
+
+        return Map.of("RESULT", "SUCCESS");
     }
 
 }
